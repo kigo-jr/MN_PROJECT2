361a6994bf87fe24a95b6c8629a12832d4ea4587
Jakub Kluk <kuba.kluk85@gmail.com>
2022-04-24 14:18:26 +0200

    first final version that meets the project requirements

--- app/__main__.py
+++ app/__main__.py
@@ -1,29 +1,88 @@
+from math import sin
 import matplotlib.pyplot as plt
 from data_structures import vector, matrix
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
 
 # TODO: a_1 = 5 + 4, N = 969, b_n = sin(n*(2+1))
 
 if __name__ == "__main__":
+
+    #  zadanie A
+    #  indeks: 172469
+    #  N = 969, a1 = 9, a2 = a3 = -1, bn = sin(n*3)
+
+    N = 969
+    b = vector([sin((i + 1) * 3) for i in range(N)])
     A = matrix([
-        [1.5, 1.1, -0.3],
-        [1.1, 1.5, 1.1],
-        [0.3, 1.1, 2.0]
+        [9 if i==j else -1 if j < i+3 and i < j+3 else 0 for i in range(N)] for j in range(N)
     ])
 
-    x = vector(
-        [-4.025, 5.825, -2.10]
-    )
+    #  zadanie B
 
-    v = vector([1, 1, 0])
-
-    time, iters, solution = A.jacobi(vector([1.0, 2.0, 1.0]))
-    print("Jacobi method:")
-    print(A * solution)
-    print(f"solution:\n{solution}")
+    time, iters, solution, residuum = A.jacobi(b)
+    print("Jacobi's method:")
+    print(f"residuum norm: {residuum.norm}")
     print(f"needed iterations: {iters}")
+    print(f"execution time: {time} s.")
 
-    time, iters, solution = A.gauss_seidl(vector([1.0, 2.0, 1.0]))
-    print("Gauss Seidel method:")
-    print(A * solution)
-    print(f"solution:\n{solution}")
+    time, iters, solution, residuum = A.gauss_seidel(b)
+    print("Gauss-Seidel's method:")
+    print(f"residuum norm: {residuum.norm}")
     print(f"needed iterations: {iters}")
+    print(f"execution time: {time} s.")
+
+    # zadanie C
+
+    b = vector([sin((i + 1) * 3) for i in range(N)])
+    A = matrix([
+        [3 if i==j else -1 if j < i+3 and i < j+3 else 0 for i in range(N)] for j in range(N)
+    ])
+
+    try:
+        time, iters, solution, residuum = A.jacobi(b)
+    except Exception:
+        print("Jacobi's method did not converge!")
+
+    try:
+        time, iters, solution, residuum = A.gauss_seidel(b)
+    except Exception:
+        print("Gauss-Seidel's method did not converge!")
+
+    # zadanie D
+
+    time, solution, residuum = A.lu_factorization(b)
+    print("LU factorization:")
+    print(f"residuum norm: {residuum.norm}")
+    print(f"execution time: {time} s.")
+
+    # zadanie E
+
+    N_vector = [100, 500, 1000, 2000, 3000]
+
+    Jacobi_times = []
+    Gauss_Seidels_times = []
+    LU_factorization_times = []
+
+    for n in N_vector:
+        b = vector([sin((i + 1) * 3) for i in range(n)])
+        A = matrix([
+            [9 if i==j else -1 if j < i+3 and i < j+3 else 0 for i in range(n)] for j in range(n)
+        ])
+
+        Jacobi_times.append(A.jacobi(b)[0])
+        Gauss_Seidels_times.append(A.gauss_seidel(b)[0])
+        LU_factorization_times.append(A.lu_factorization(b)[0])
+
+    plt.plot(N_vector, Jacobi_times, label="Jacobi", color="red")
+    plt.plot(N_vector, Gauss_Seidels_times, label="Gauss-Seidel", color="blue")
+    plt.plot(N_vector, LU_factorization_times, label="LU factorization", color="green")
+    plt.grid(True)
+    plt.legend()
+    plt.ylabel(r"Czas [$s$]")
+    plt.xlabel("Liczba niewiadomych")
+    plt.title("Zależność czasu wykonywania algorytmów od liczby niewiadomych")
+    plt.show()
--- app/data_structures/matrix.py
+++ app/data_structures/matrix.py
@@ -1,11 +1,39 @@
 from typing import List, Tuple, Union
 from data_structures.vector import vector
+from time import time
 
 
 class matrix:
 
     """
     Klasa reprezentująca macierz.
+    -----------------------------
+
+    zawartość klasy:
+    - inicjalizacja na podstawie listy list reprezentującej macierz
+
+    >>> A = matrix([
+        [1.0, 0.0, 0.0],
+        [0.0, 2.0, 0.0],
+        [0.0, 0.0, 3.0]
+    ])
+
+    - podstawowe operacje arytmetyczne na macierzach (dodawanie, odejmowanie, iloczyn z wektorem)
+
+    >>> C = A - B
+    >>> C = A + B
+    >>> y = A * x
+
+    - metody rozwiązywania układów równać metodami Jacobiego, Gaussa-Seidela, faktoryzacji LU
+
+    >>> time, iterations, solution, residuum = A.jacobi(vector([1.0, 2.0, 3.0]))
+    >>> time, iterations, solution, residuum = A.gauss_seidel(vector([1.0, 2.0, 3.0]))
+    >>> time, solution, residuum = A.lu_factorization(vector([1.0, 2.0, 3.0]))
+
+    - metody generujące macierze kwadratowe jednostkowe oraz zerowe o zadanym wymiarze
+
+    >>> I = matrix.ones(7)
+    >>> Z = matrix.zeros(7)
     """
 
     def __init__(self, array: Union[List[List[float]], None]=None) -> None:
@@ -122,13 +150,19 @@ class matrix:
     def ones(n: int) -> "matrix":
         return matrix([[1.0 if i == j else 0.0 for i in range(n)] for j in range(n)])
 
+    @staticmethod
+    def zeros(n: int) -> "matrix":
+        return matrix([[0.0 for _ in range(n)] for _ in range(n)])
+
     def copy(self) -> "matrix":
         copy_list = [[element for element in row] for row in self.matrix]
         return matrix(copy_list)
 
-    def jacobi(self, b: vector, inplace: bool=False) -> Tuple[float, int, vector]:
+    def jacobi(self, b: vector, precision: float=1e-9, inplace: bool=False) -> Tuple[float, int, vector, vector]:
         if self.cols != len(b):
             raise Exception("")
+
+        start_time: float = time()
 
         A_copy: "matrix" = self.copy() if not inplace else self
 
@@ -150,14 +184,19 @@ class matrix:
 
             iterations += 1
 
-            if res.norm < 1e-9:
+            if iterations > 100:
+                raise Exception("Iteration count exceeded 10 000!")
+
+            if res.norm < precision:
                 break
         # TODO: measure time in Jacobi's method
-        return (0.0, iterations, x)
+        return (time() - start_time, iterations, x, res)
 
-    def gauss_seidl(self, b: vector, inplace: bool=False) -> Tuple[float, int, vector]:
+    def gauss_seidel(self, b: vector, precision: float=1e-9, inplace: bool=False) -> Tuple[float, int, vector, vector]:
         if self.cols != len(b):
             raise Exception("")
+
+        start_time: float = time()
 
         A_copy: "matrix" = self.copy() if not inplace else self
 
@@ -177,11 +216,48 @@ class matrix:
 
             iterations += 1
 
-            if res.norm < 1e-9:
+            if iterations > 100:
+                raise Exception("Iteration count exceeded 10 000!")
+
+            if res.norm < precision:
                 break
         # TODO: measure time in Gauss Seidel's method
-        return (0.0, iterations, x)
+        return (time() - start_time, iterations, x, res)
 
-    def lu_factorization(self, b: vector, inplace: bool=False) -> Tuple[float, int, vector]:
+    def lu_factorization(self, b: vector, inplace: bool=False) -> Tuple[float, vector, vector]:
         # TODO: implement LU factorization
-        pass
+        start_time: float = time()
+        A_copy: "matrix" = self.copy() if not inplace else self
+        L: "matrix" = matrix.ones(A_copy.cols)
+        U: "matrix" = matrix.zeros(A_copy.cols)
+
+        x: vector = vector.ones(A_copy.cols)
+        y: vector = vector.zeros(A_copy.cols)
+
+        for j in range(A_copy.cols):
+            for i in range(j+1):
+                U[i][j] = A_copy[i][j]
+                for k in range(i):
+                    U[i][j] -= L[i][k] * U[k][j]
+
+            for i in range(j+1, A_copy.cols):
+                for k in range(j):
+                    L[i][j] -= L[i][k] * U[k][j]
+                L[i][j] += A_copy[i][j]
+                L[i][j] /= U[j][j]
+
+        for i in range(A_copy.cols):
+            value: float = b[i]
+            for j in range(i):
+                value -= L[i][j] * y[j]
+            y[i] = value / L[i][i]
+
+        for i in range(A_copy.cols - 1, -1, -1):
+            value: float = y[i]
+            for j in range(i + 1, A_copy.cols):
+                value -= U[i][j] * x[j]
+            x[i] = value / U[i][i]
+
+        res: vector = A_copy*x - b
+
+        return (time() - start_time, x, res)
